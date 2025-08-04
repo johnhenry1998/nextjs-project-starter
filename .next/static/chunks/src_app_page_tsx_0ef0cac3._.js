1027ae06000d02a30c4e6be84d3af4c737c9ddab
johnhenry1998 <36958950+johnhenry1998@users.noreply.github.com>
2025-08-04 19:42:15 +0700

    Add .next/static/chunks/src_app_page_tsx_0ef0cac3._.js 1754311335488

---
+++ .next/static/chunks/src_app_page_tsx_0ef0cac3._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_lodash_90f72504._.js",
+  "static/chunks/node_modules_recharts_es6_29f6c739._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_c50bde01._.js",
+  "static/chunks/src_c5e27259._.js"
+],
+    source: "dynamic"
+});
